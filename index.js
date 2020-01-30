5e548b64f9713b2829aaf6bae8d4d7ef900406c8
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-01-29 20:33:01 -0500

    User login, register, and logout routes are working as intended

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
+
+const dbConfig = require("./database/dbConfig");
+const authRouter = require("./auth/auth-router");
+const usersRouter = require("./users/users-router");
+
+const server = express();
+const port = process.env.PORT || 5500;
+
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+server.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: "keep it secret, keep it safe!",
+    cookie: {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      secure: false // in prod this should be true so the cookie header is encrypted
+    },
+    store: new KnexSessionStore({
+      knex: dbConfig,
+      createtable: true
+    })
+  })
+);
+
+server.use("/auth", authRouter);
+server.use("/users", usersRouter);
+
+server.get("/", (req, res, next) => {
+  res.json({
+    message: "Welcome to our API"
+  });
+});
+
+server.use((err, req, res, next) => {
+  console.log("Error:", err);
+
+  res.status(500).json({
+    message: "Something went wrong"
+  });
+});
+
+server.listen(port, () => {
+  console.log(`\n** Running on http://localhost:${port} **\n`);
+});
