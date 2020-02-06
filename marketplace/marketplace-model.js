50e83b00798e370f0335f13ee0bb710f1a868baf
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-02-05 19:19:36 -0500

    Added update and delete functionality

---
+++ marketplace/marketplace-model.js
@@ -0,0 +1,25 @@
+const db = require("../database/dbConfig");
+
+function findById(id) {
+  return db("products")
+    .where({ id })
+    .first();
+}
+
+async function update(id, body) {
+  await db("products")
+    .where({ id })
+    .update(body);
+
+  return findById(id);
+}
+
+function remove(id) {
+  return db("products").where({ id }).del;
+}
+
+module.exports = {
+  findById,
+  update,
+  remove
+};
