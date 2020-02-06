03815cd292b3c5a3b39e9ad003d6e08b7b519dd8
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-02-06 17:22:44 -0500

    Added middleare and token service for jwt

--- users/users-router.js
+++ users/users-router.js
@@ -1,17 +1,23 @@
-const express = require("express");
-const restricted = require("../middleware/restricted");
-const usersModel = require("./users-model");
+const router = require("express").Router();
 
-const router = express.Router();
+const Users = require("../users/users-model.js");
+const restricted = require("../auth/restricted-middleware.js");
+const checkRole = require("../auth/check-role-middleware.js");
 
-router.get("/", restricted(), async (req, res, next) => {
-  try {
-    const users = await usersModel.find();
+router.get("/", restricted, checkRole("Student"), (req, res) => {
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
 
-    res.json(users);
-  } catch (err) {
-    next(err);
-  }
+router.get("/:id", restricted, checkRole("Student"), (req, res) => {
+  Users.findById(req.params.id)
+    .then(user => {
+      res.json(user);
+    })
+    .catch(err => res.send(err));
 });
 
 module.exports = router;
