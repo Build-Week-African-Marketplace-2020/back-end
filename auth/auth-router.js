cc524f0f6320b672ae094597996cf3f60f66c54f
Aaron Spurgeon <aspurgeon061@gmail.com>
2020-02-06 18:31:42 -0500

    Server fix

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,18 +4,25 @@ const bcrypt = require("bcryptjs");
 const tokenService = require("../auth/token-service.js");
 const Users = require("../users/users-model.js");
 
-router.post("/register", (req, res) => {
-  let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10);
-  user.password = hash;
+router.post("/register", async (req, res, next) => {
+  // let user = req.body;
+  // const hash = bcrypt.hashSync(user.password, 10);
+  // user.password = hash;
 
-  Users.add(user, "id")
-    .then(saved => {
-      res.status(201).json(saved);
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
+  // Users.add(user, "id")
+  //   .then(saved => {
+  //     res.status(201).json(saved);
+  //   })
+  //   .catch(error => {
+  //     res.status(500).json(error);
+  //   });
+  try {
+    const saved = await Users.add(req.body, "id");
+
+    res.status(201).json(saved);
+  } catch (err) {
+    next(err);
+  }
 });
 
 router.post("/login", (req, res) => {
